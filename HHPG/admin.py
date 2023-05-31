9f7ec58464c959b6d856880d081cead8a8d9783d
Lukas.Hoernle <Lukas.Hoernle@cas.de>
2023-05-31 20:23:48 +0200

    automatic python additions

--- HHPG/admin.py
+++ HHPG/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .domain.entity.haushaltsposten import Haushaltsposten
+from .domain.entity.projekt import Project
+
+admin.site.register(Haushaltsposten)
+admin.site.register(Project)
