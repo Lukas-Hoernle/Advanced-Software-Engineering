17d99c028741bbf41664ee435aa132f23b1dc510
Lukas Hörnle <l.hoernle@stuv-karlsruhe.de>
2023-07-12 11:36:37 +0000

    added HaushaltsplanExcelGenerator.py

---
+++ HHPG/application/HaushaltsplanExcelGenerator.py
@@ -0,0 +1,22 @@
+import openpyxl
+
+class HaushaltsplanExcelGenerator:
+    def generate_excel_poorly(self, haushaltsplan, file_name):
+        workbook = openpyxl.Workbook()
+        worksheet = workbook.active
+
+        worksheet['A1'] = 'Projekt Name'
+        worksheet['B1'] = 'Einnahmen'
+        worksheet['C1'] = 'Ausgaben'
+
+        row_index = 2
+
+        for haushaltsposten in haushaltsplan.haushaltsposten:
+            worksheet.cell(row=row_index, column=1, value=haushaltsposten.name)
+
+            for projekt in haushaltsposten.projekte:
+                worksheet.cell(row=row_index, column=2, value=projekt.einnahmen)
+                worksheet.cell(row=row_index, column=3, value=projekt.ausgaben)
+                row_index += 1
+
+        workbook.save(file_name)
