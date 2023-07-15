676cf784e53c83f92222a3c5aab11b5c6f9ff343
Lukas.Hoernle <Lukas.Hoernle@cas.de>
2023-07-15 20:09:44 +0200

    added factorypattern as formfactories

---
+++ HHPG/application/forms/formfactories.py
@@ -0,0 +1,30 @@
+from django.forms import inlineformset_factory
+
+from HHPG.domain.entity.aufwand import Aufwand
+from HHPG.domain.entity.haushaltsplan import Haushaltsplan
+from HHPG.domain.entity.haushaltsposten import Haushaltsposten
+from HHPG.domain.entity.projekt import Projekt
+
+HaushaltspostenFormSet = inlineformset_factory(
+    Haushaltsplan,
+    Haushaltsposten,
+    fields=['name'],
+    extra=1,
+    can_delete=True
+)
+
+ProjektFormSet = inlineformset_factory(
+    Haushaltsposten,
+    Projekt,
+    fields=['name'],
+    extra=1,
+    can_delete=True
+)
+
+AufwandFormSet = inlineformset_factory(
+    Projekt,
+    Aufwand,
+    fields=['einnahmen', 'ausgaben'],
+    extra=1,
+    can_delete=True
+)
