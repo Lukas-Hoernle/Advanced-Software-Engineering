7cd8e80114438f83325e3db1538a84799eea7e66
Lukas.Hoernle <Lukas.Hoernle@cas.de>
2023-07-21 10:23:28 +0200

    redid tests

--- HHPG/infrastruktur/test_projekt_repository.py
+++ HHPG/infrastruktur/test_projekt_repository.py
@@ -2,32 +2,7 @@ from unittest import TestCase
 
 
 class TestProjektRepository(TestCase):
-    def test_get_by_id(self):
-        self.test_get_by_id()
-    def test_get_count(self):
-        self.test_get_count()
+    def setUp(self):
 
-    def test_get_all_by_haushaltsposten(self):
-        self.test_get_all_by_haushaltsposten()
-
-    def test_get_all_by_haushaltsposten_including_children(self):
-        self.test_get_all_by_haushaltsposten_including_children()
-
-    def test_get_all_by_name(self):
-        self.test_get_all_by_name()
-
-    def test_get_all_by_einnahmen(self):
-        self.test_get_all_by_einnahmen()
-
-    def test_get_haushaltsposten(self):
-        self.test_get_haushaltsposten()
-
-    def test_get_name(self):
-        self.test_get_name()
-
-    def test_get_einnahmen(self):
-        self.test_get_einnahmen()
-
-    def test_get_ausgaben(self):
-        self.test_get_ausgaben()
-
+    def get_by_id(self):
+        self.assertEqual(self,self.get_by_id(self.id()))
