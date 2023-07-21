5ec70448903ff85c612fb15da5880a2ffd0b112d
Lukas.Hoernle <Lukas.Hoernle@cas.de>
2023-07-21 10:29:17 +0200

    fixed setup in first redone test

--- HHPG/infrastruktur/test_projekt_repository.py
+++ HHPG/infrastruktur/test_projekt_repository.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
 
+from HHPG.domain.entity.projekt import Projekt
+
 
 class TestProjektRepository(TestCase):
     def setUp(self):
-
+        Projekt.__new__()
     def get_by_id(self):
         self.assertEqual(self,self.get_by_id(self.id()))
