b95b3cb6938da140733df86935014e2909d255e3
Lukas.Hoernle <Lukas.Hoernle@cas.de>
2023-07-21 10:39:47 +0200

    redoing tests

--- HHPG/test/test_projekt_repository.py
+++ HHPG/test/test_projekt_repository.py
@@ -1,10 +1,12 @@
 from unittest import TestCase
-
 from HHPG.domain.entity.projekt import Projekt
 
 
 class TestProjektRepository(TestCase):
     def setUp(self):
-        a = Projekt.__new__(Projekt)
-    def get_by_id(self,a):
-        self.assertEqual(a,self.get_by_id(a.id()))
+        self.projekt = Projekt.objects.create(projekt_name="Testprojekt")
+
+    def test_get_by_id(self):
+        retrieved_projekt = Projekt.objects.get_by_id(self.projekt.get_id)
+
+        self.assertEqual(self.projekt, retrieved_projekt)
