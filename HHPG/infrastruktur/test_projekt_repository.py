490045157d02425097ac4e8a91c9ae3f2f91c814
Lukas.Hoernle <Lukas.Hoernle@cas.de>
2023-07-21 10:32:09 +0200

    fixing first redone test

--- HHPG/infrastruktur/test_projekt_repository.py
+++ HHPG/infrastruktur/test_projekt_repository.py
@@ -5,6 +5,6 @@ from HHPG.domain.entity.projekt import Projekt
 
 class TestProjektRepository(TestCase):
     def setUp(self):
-        Projekt.projectA = Projekt.__new__()
-    def get_by_id(self):
-        self.assertEqual(Projekt.projectA,self.get_by_id(Projekt.projectA.id()))
+        a= Projekt.__new__()
+    def get_by_id(self,a):
+        self.assertEqual(a,self.get_by_id(a.id()))
